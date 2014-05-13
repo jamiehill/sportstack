09281d30bd5bfe27dc3952b551b04d6b404fd15a
James Hill <jhill@amelco.co.uk>
2014-05-13 16:43:49 +0100

    formatting

--- sportstack.js
+++ sportstack.js
@@ -1,38 +1,38 @@
 /**
  * Created by Jamie on 09/05/2014.
  */
-(function($) {
+ (function($) {
 
 
-	var SportStack = function(elem, options) {
-		var $element = elem, plugin = this, stacks = {};
-		var defaults = {
-			childClass: 'child',
-			expandedClass: 'xpanded',
-	        hideSpeed: 1000,
-	        showSpeed: 1000
-		}
+ 	var SportStack = function(elem, options) {
+ 		var $element = elem, plugin = this, stacks = {};
+ 		var defaults = {
+ 			childClass: 'child',
+ 			expandedClass: 'xpanded',
+ 			hideSpeed: 1000,
+ 			showSpeed: 1000
+ 		}
 
 		/**
 		 * Initialise the component
 		 * @return {[type]} [description]
 		 */
-        var init = function() {
-        	var metadata = $element.data( 'plugin-options' )
-        	plugin.config = $.extend({}, defaults, options, metadata);
-    		parseElement($element);
-    	};
+		 var init = function() {
+		 	var metadata = $element.data( 'plugin-options' )
+		 	plugin.config = $.extend({}, defaults, options, metadata);
+		 	parseElement($element);
+		 };
 
-    	plugin.toggle = function(el, index) {
-			var data = stacks[$(el).attr('id')];
-			if (index >= 0 && index <= data.blocks.length - 1) {
+		 plugin.toggle = function(el, index) {
+		 	var data = stacks[$(el).attr('id')];
+		 	if (index >= 0 && index <= data.blocks.length - 1) {
 
-				var settings = data.settings[index],
-					method = settings.collapsed ? 'show' : 'hide';
+		 		var settings = data.settings[index],
+		 		method = settings.collapsed ? 'show' : 'hide';
 
-				plugin[method].apply(plugin, [el, index]);
-			}
-    	};
+		 		plugin[method].apply(plugin, [el, index]);
+		 	}
+		 };
 
     	/**
     	 * Show the specified element
@@ -40,13 +40,13 @@
     	 * @param  {[type]} index [description]
     	 * @return {[type]}       [description]
     	 */
-    	plugin.show = function(el, index) {
-    		var data = stacks[$(el).attr('id')];
-    		if (index >= 0 && index <= data.blocks.length - 1) {
+    	 plugin.show = function(el, index) {
+    	 	var data = stacks[$(el).attr('id')];
+    	 	if (index >= 0 && index <= data.blocks.length - 1) {
 
-                var $header 	= $(data.headers[index]),
-                    $block  	= $(data.blocks[index]),
-                    settings   	= data.settings[index];
+    	 		var $header 	= $(data.headers[index]),
+    	 		$block  	= $(data.blocks[index]),
+    	 		settings   	= data.settings[index];
 
                 if (!settings.collapsed) // toggle closed
                 	return plugin.hide(el, index);
@@ -64,7 +64,7 @@
 
                 // $block.css({'display':'block'});
             }
-    	};
+        };
 
     	/**
     	 * Hide the specified element
@@ -72,18 +72,18 @@
     	 * @param  {[type]} index [description]
     	 * @return {[type]}       [description]
     	 */
-    	plugin.hide = function(el, index) {
-    		var data = stacks[$(el).attr('id')];
-    		if (index >= 0 && index <= data.blocks.length - 1) {
+    	 plugin.hide = function(el, index) {
+    	 	var data = stacks[$(el).attr('id')];
+    	 	if (index >= 0 && index <= data.blocks.length - 1) {
 
-                var $header 	= $(data.headers[index]),
-                    $block 		= $(data.blocks[index]),
-                    settings	= data.settings[index];
+    	 		var $header 	= $(data.headers[index]),
+    	 		$block 		= $(data.blocks[index]),
+    	 		settings	= data.settings[index];
 
-                $header.removeClass(plugin.config.expandedClass);
-                $block.removeClass(plugin.config.expandedClass);
-                settings.collapsed = true;
-                
+    	 		$header.removeClass(plugin.config.expandedClass);
+    	 		$block.removeClass(plugin.config.expandedClass);
+    	 		settings.collapsed = true;
+    	 		
                 // $block.stop();
                 // $block.css({'visibility': 'hidden'});
 
@@ -98,97 +98,97 @@
                 // });
 
 				// $block.css({'display':'none'})
-            }
-    	};
+			}
+		};
 
     	/**
     	 * Parses a 'sportstack' element
     	 * @param  {[type]} el [description]
     	 * @return {[type]}    [description]
     	 */
-    	var parseElement  = function(el) {
-			var elementId = ensureUid(el),
-    			content   = parseContent(el);
+    	 var parseElement  = function(el) {
+    	 	var elementId = ensureUid(el),
+    	 	content   = parseContent(el);
 
-    		stacks[elementId] = {
-				headers:  parseHeaders(el),
-				blocks:   content.blocks,
-				settings: content.settings
-			}
-    	}
+    	 	stacks[elementId] = {
+    	 		headers:  parseHeaders(el),
+    	 		blocks:   content.blocks,
+    	 		settings: content.settings
+    	 	}
+    	 }
 
     	/**
     	 * Walks all headers
     	 * @param  {[type]} el [description]
     	 * @return {[type]}    [description]
     	 */
-    	var parseHeaders = function(el) {
-    		var headers = $('dt', el);
-    		headers.each(function(index) {
-	        	$(this).bind('click', function() {
-	                plugin.toggle(el, index);
-	            });
-	        });
+    	 var parseHeaders = function(el) {
+    	 	var headers = $('dt', el);
+    	 	headers.each(function(index) {
+    	 		$(this).bind('click', function() {
+    	 			plugin.toggle(el, index);
+    	 		});
+    	 	});
 
-	        return headers;
-    	};
+    	 	return headers;
+    	 };
 
     	/**
     	 * Walks all content blocks
     	 * @param  {[type]} el [description]
     	 * @return {[type]}    [description]
     	 */
-    	var parseContent = function(el) {
-    		var blocks = $('dd', el),
-				settings = [];
+    	 var parseContent = function(el) {
+    	 	var blocks = $('dd', el),
+    	 	settings = [];
 
-	        blocks.each(function(index) {
-	            var $block = $(this);
-	            settings.push({
-	                'height':      $block.height(),
-	                'outerHeight': $block.outerHeight(),
-	                'padding':     zerofy($block.css('padding')),
-	                'margin':      zerofy($block.css('margin')), 
-	                'collapsed':   false            
-	            });
-	        });
+    	 	blocks.each(function(index) {
+    	 		var $block = $(this);
+    	 		settings.push({
+    	 			'height':      $block.height(),
+    	 			'outerHeight': $block.outerHeight(),
+    	 			'padding':     zerofy($block.css('padding')),
+    	 			'margin':      zerofy($block.css('margin')), 
+    	 			'collapsed':   false            
+    	 		});
+    	 	});
 
-	        return {blocks:blocks, settings:settings};
-    	};
+    	 	return {blocks:blocks, settings:settings};
+    	 };
 
     	/**
     	 * Adds a unique id attribute to the element if it doesn't have an id already
     	 * @type {Number}
     	 */
-    	var uid_counter = 0;
-    	var ensureUid = function(el) {
-    		var id = el.attr('id');
-        	if (id === undefined) {
-				id = ('sportstack'+uid_counter ++);
-				el.attr('id', id);
-			};
-			return id;
-		}
+    	 var uid_counter = 0;
+    	 var ensureUid = function(el) {
+    	 	var id = el.attr('id');
+    	 	if (id === undefined) {
+    	 		id = ('sportstack'+uid_counter ++);
+    	 		el.attr('id', id);
+    	 	};
+    	 	return id;
+    	 }
 
 		/**
 		 * Helper to zerofy a value
 		 * @param  {[type]} value [description]
 		 * @return {[type]}       [description]
 		 */
-    	var zerofy = function(value) {
-	        value = parseInt(value, 10);
-	        return isNaN(value) ? 0 : value;
-	    };
+		 var zerofy = function(value) {
+		 	value = parseInt(value, 10);
+		 	return isNaN(value) ? 0 : value;
+		 };
 
-	    init();
-    };
-
-
-    $.fn.sportstack = function(options) {
-	    return this.each(function() {
-	      	new SportStack($(this), options);
-	    });
-	};
+		 init();
+		};
 
 
-}(jQuery));
+		$.fn.sportstack = function(options) {
+			return this.each(function() {
+				new SportStack($(this), options);
+			});
+		};
+
+
+	}(jQuery));
